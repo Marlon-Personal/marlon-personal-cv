b33f7f52a5d0d4df0c41e7aa40da7191ce02b958
Marlon <marlon.aguero.vargas@gmail.com>
2023-03-16 22:17:43 -0600

    Fixed mobile bugs
    On this commit:
    - Fixed mobile bugs for cardCarouselExtended

--- src/components/CardCarouselExtended.js
+++ src/components/CardCarouselExtended.js
@@ -26,11 +26,11 @@ const CardCarousel = ({ data, sectionTitle, isReverse }) => {
 
     return (
         <>
-            <div id="card-carousel" className="md:p-24 pb-16 h-screen flex flex-col justify-center">
+            <div id="card-carousel" className="md:p-24 py-10 h-screen flex flex-col justify-center">
                 <h2 className="animate-fade-in text-center text-4xl text-gray-800 hidden md:block">{data.title}</h2>
                 <div class={`flex ${isReverse ? 'md:flex-row-reverse' : 'md:flex-row'} flex-col-reverse justify-end md:justify-center md:items-center h-full`}>
-                    <div class="p-12 w-full h-auto flex flex-col justify-start md:justify-around">
-                    <h3 className="mb-5 text-lg font-light tracking-tight text-gray-500 text-center md:text-left">{data.dates}</h3>       
+                    <div class="p-12 w-full h-3/4 md:h-auto flex flex-col justify-start md:justify-around">
+                        <h3 className="mb-5 text-lg font-light tracking-tight text-gray-500 text-center md:text-left">{data.dates}</h3>
                         <div className="md:animate-fade-in justify-between mb-6 md:hidden flex">
                             <a onClick={carouselInfiniteScrollBack} className="text-xl md:text-3xl text-gray-500 hover:text-gray-900"><BsArrowLeft /></a>
                             <a onClick={carouselInfiniteScroll} className="text-xl md:text-3xl text-gray-500 hover:text-gray-900"><BsArrowRight /></a>
@@ -51,8 +51,9 @@ const CardCarousel = ({ data, sectionTitle, isReverse }) => {
                             <a onClick={carouselInfiniteScroll} className="text-xl md:text-3xl text-gray-500 hover:text-gray-900"><BsArrowRight /></a>
                         </div>
                     </div>
-                    <div style={{ backgroundImage: `url(${data.image})` }} className="bg-center bg-cover md:animate-fade-in h-2/3 md:h-3/4 w-full flex items-center justify-center md:mt-0">
-                        <h2 className="animate-fade-in text-center text-5xl text-white block bg-gray-900/50 w-full py-4 md:hidden">{sectionTitle}</h2>
+
+                    <div style={{ backgroundImage: `url(${data.image})` }} className="bg-center bg-cover md:animate-fade-in h-1/4 md:h-3/4 w-full flex items-center justify-center md:mt-0">
+                        <h2 className="animate-fade-in text-center text-4xl text-white block bg-gray-900/50 w-full py-4 md:hidden">{data.title}</h2>
                     </div>
                 </div>
             </div>
