8a51d24ed552aeb6dc89e5f48f7df7fd05b16f97
Marlon <marlon.aguero.vargas@gmail.com>
2023-03-16 22:59:09 -0600

    Hero banner
    On this commit:
    - Hero banner update for blog and jobs pages.
    - Made id for section dynamic for CardCarouselExtended

--- src/pages/ArticleListPage.js
+++ src/pages/ArticleListPage.js
@@ -8,7 +8,7 @@ const ArticlesListPage = () => {
     return (
         <>
             <NavBar />
-            <HeroComponent title="Welcome to my personal blog" paragragh="Lorem ipsum dolor amet sit" button="Click here" button_url="#articles-body" backgroundImage="https://images.unsplash.com/photo-1437419764061-2473afe69fc2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2232&q=80" />
+            <HeroComponent title="Welcome to my personal blog" paragragh="Lorem ipsum dolor amet sit" button="Click here" button_url="#articles-body" backgroundImage="https://images.unsplash.com/photo-1505682634904-d7c8d95cdc50?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80" />
         
             <ArticleList articles={articles} />
             <Footer/>
