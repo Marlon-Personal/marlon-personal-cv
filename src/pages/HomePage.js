09d8e1ca8257ecf52eac5fd6c294997e3f32f958
Marlon <marlon.aguero.vargas@gmail.com>
2023-03-17 15:44:18 -0600

    Fixes
    On this commit:
    -Updated content to most of the sections
    -Created and about file to save the about info
    -Added tech-skills file to save tech skills and soft skills separately
    -Update NotFound template
    -Updated a few styles and scroll animation for iconsblocks

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,23 +6,17 @@ import HeroComponent from '../components/HeroComponent';
 import IconsBlocks from '../components/IconsBlocks';
 import NavBar from '../components/NavBar';
 import jobs from '../data/jobs'
-import skills from '../data/skills'
+import techSkills from '../data/tech-skills'
+import aboutMeData from '../data/about';
 
 function Home() {
-
-    console.log(jobs);
-    const aboutMeData = [
-        { title: "About me", description: "My name is Marlon, I am a Software Developer working in Frontend for over 3 years. I live in Costa Rica working remotely for an e-commerce company based in UK.", image: "https://images.unsplash.com/photo-1570215171323-4ec328f3f5fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" },
-        { title: "My hobbies", description: "My hobbies are soccer, nature and macro photography, building legos and solving puzzlez, video games and taking care of my plants!", image: "https://images.unsplash.com/photo-1611329857570-f02f340e7378?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" },
-        { title: "My family", description: "I live with my beautiful wife and my 3 cats Millies, who is the older, wiser and lazier. George Michael, the alpha male (according to him), and Beanie, a cute little ball of fur. We live outside of the capital in a small town", image: "https://images.unsplash.com/photo-1568043210943-0e8aac4b9734?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" }
-    ]
 
     return (
         <>
             <NavBar />
-            <HeroComponent title="Hello world! I am Marlon" paragragh="Lorem ipsum dolor amet sit" button="Check more about me!" button_url="#card-carousel" backgroundImage="https://images.unsplash.com/photo-1578662996442-48f60103fc96?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80" />
+            <HeroComponent title="Hello world! I am Marlon" button="Check more about me!" button_url="#card-carousel" backgroundImage="https://images.unsplash.com/photo-1578662996442-48f60103fc96?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80" />
             <CardCarousel data={aboutMeData} sectionTitle="About Me" arrowLink="#icon-blocks" />
-            <IconsBlocks data={skills} sectionTitle="Skills I've picked up along the way" />
+            <IconsBlocks data={techSkills} sectionTitle="Skills I've picked up along the way" />
             <CardDropdown mainImage={"https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80"} data={jobs.compresedData} sectionTitle="My past jobs" />
             <Footer />
         </>
--- src/pages/MyJobsPage.js
+++ src/pages/MyJobsPage.js
@@ -11,7 +11,7 @@ const MyJobsPage = () => {
         <>
         <NavBar/>
         <div>
-            <HeroComponent title="My jobs" paragragh="Lorem ipsum dolor amet sit" button="Check my jobs!" button_url="#carouselJob0" backgroundImage="https://images.unsplash.com/photo-1662147238273-e1fe1cdb693b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80" />
+            <HeroComponent title="My jobs" button="Check my jobs!" button_url="#carouselJob0" backgroundImage="https://images.unsplash.com/photo-1662147238273-e1fe1cdb693b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80" />
             <CardCarouselExtended data={jobs.extendedData[0]} idCarousel={'carouselJob0'} isReverse={false}/>
             <CardCarouselExtended data={jobs.extendedData[1]} idCarousel={'carouselJob1'} isReverse={true}/>
             <CardCarouselExtended data={jobs.extendedData[2]} idCarousel={'carouselJob2'} isReverse={false}/>
